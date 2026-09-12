6d0ec46bebc73e64d94ac44cb9ac6a825f6dbf8d
Bishal <bishaladhikari872@gmail.com>
2026-09-12 17:43:39 +0545

    chore: Configured backend for deployment, created a docker file for easier depolyment

--- server/src/main/java/com/watchstore/server/config/SecurityConfig.java
+++ server/src/main/java/com/watchstore/server/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.watchstore.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        .cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable()) // disable CSRF for testing
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
